8cb0fadbedae2c1708b8165e014bc7390e4bee51
Gabriel <gabriel_aires@estudante.sesisenai.org.br>
2026-04-09 19:21:26 -0300

    Script o Banco de dados

---
+++ banco/script.sql
@@ -0,0 +1,17 @@
+-- Cria o Banco de Dados caso ele ainda não exista
+CREATE DATABASE IF NOT EXISTS bibliotech_db;
+
+-- Seleciona o banco de dados
+USE bibliotech_db;
+
+-- Tabela de livros:
+CREATE TABLE IF NOT EXISTS livros
+(
+    id INT NOT NULL AUTO_INCREMENT,
+    autor VARCHAR(255) NOT NULL,
+    titulo VARCHAR(255) NOT NULL,
+    ano_publicacao INT NOT NULL,
+    genero VARCHAR(255) NOT NULL,
+    capa VARCHAR(255),
+    PRIMARY KEY (id)
+)
